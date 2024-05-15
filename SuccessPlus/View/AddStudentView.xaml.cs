0fc834e1a956888865002c11b700b51eedc208c3
sasha <sasha@HOME-PC>
2024-05-16 00:47:43 +0500

    добавление студентов, удаление лишних строк датагрид и цветов

--- SuccessPlus/View/AddStudentView.xaml.cs
+++ SuccessPlus/View/AddStudentView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuccessPlus.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,37 @@ namespace SuccessPlus.View
     /// <summary>
     /// Логика взаимодействия для AddStudentView.xaml
     /// </summary>
-    public partial class AddStudentView : Page
+    public partial class AddStudentView : Window
     {
+        public Core db = new Core();
+        List<Group> GroupList { get; set; } = new List<Group>();
         public AddStudentView()
         {
             InitializeComponent();
+            GroupList = db.context.Group.ToList();
+            GroupComboBox.ItemsSource = GroupList;
+            GroupComboBox.SelectedValuePath = "IdGroup";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student
+            {
+                FisrtName = FirstName.Text,
+                LastName = LastName.Text,
+                GroupId = (int)GroupComboBox.SelectedValue
+            };
+            try
+            {
+                db.context.Student.Add(student);
+                db.context.SaveChanges();
+                if (db.context.SaveChanges() == 0)
+                    MessageBox.Show("Успешно");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
--- SuccessPlus/View/Students.xaml.cs
+++ SuccessPlus/View/Students.xaml.cs
@@ -8,10 +8,10 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SuccessPlus.View
@@ -21,9 +21,10 @@ namespace SuccessPlus.View
     /// </summary>
     public partial class Students : Page
     {
-        public List<Student> StudentList {  get; set; } = new List<Student>();
+        public List<Student> StudentList { get; set; } = new List<Student>();
 
         private Core _db = new Core();
+        public int SelectedStudentId { get; set; } 
         public Students()
         {
 
@@ -31,7 +32,35 @@ namespace SuccessPlus.View
             StudentList = _db.context.Student.ToList();
             DataGrid.ItemsSource = StudentList;
 
-           
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddStudentView addStudentView = new AddStudentView();
+            addStudentView.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            DialogResult result = System.Windows.Forms.MessageBox.Show(
+        "Вы уверенны,что хотите удалить?",
+        "Удаление студента",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Exclamation,
+        MessageBoxDefaultButton.Button2
+        );
+
+            if (result == DialogResult.Yes)
+            {
+                Student selectedStudent = _db.context.Student.Where(x => x.IdStudent == (int)DataGrid.SelectedValue).FirstOrDefault();
+                _db.context.Student.Remove(selectedStudent);
+                _db.context.SaveChanges();
+                if (_db.context.SaveChanges() == 0)
+                    System.Windows.MessageBox.Show("Удалено");
+
+            }
+
         }
     }
 }
